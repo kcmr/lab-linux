57b077dbc8097551fe0d7558600fd0899553f971
Kus Cámara <kcmr@users.noreply.github.com>
2022-10-17 12:16:07 +0200

    check if word param has been passed before using curl

--- ejercicio4.sh
+++ ejercicio4.sh
@@ -4,13 +4,13 @@ url=https://example.org/
 word=$1
 output_file=output/ejercicio4
 
-curl $url --create-dirs -so $output_file
-grep -qiw $word < $output_file
-
 if [ $? -eq 1 ]; then
   echo "No se ha encontrado la palabra \"$word\""
   exit 1
 fi
+
+curl $url --create-dirs -so $output_file
+grep -qiw $word < $output_file
 
 total_matches=$(grep -iwc $word < $output_file)
 first_line_number_match=$(grep -iwn $word < $output_file | grep -Eo '^[[:digit:]]*' | head -1)
